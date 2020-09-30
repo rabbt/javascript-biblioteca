5d524d484ebdec27e1cd2ac280cd54bd16f8d4a2
rabbt <albertnio@live.com.mx>
2020-09-30 12:34:06 -0500

    cambios app clima

---
+++ 36-PROYECTO-Clima/js/app.js
@@ -0,0 +1,50 @@
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.addEventListener('load',() => {
+    formulario.addEventListener('submit',buscarClima);
+})
+
+function buscarClima(e) {
+    e.preventDefault();
+
+    //validar
+    const ciudad = document.querySelector('#ciudad').value
+    const pais = document.querySelector('#pais').value
+
+    if (ciudad === '' || pais === ''){
+        mostrarError('ambos campos son obligatorios') //llamar una funcion
+
+        return;
+    }
+
+    //consultariamos la api
+    consultarAPI(ciudad, pais);
+}
+function mostrarError(mensaje){
+
+    const alerta = document.querySelector('.bg-red-100')
+
+    if(!alerta){ //si la alerta nos regresa vacio ejecuta el codigo (para evitar que se repita)
+        const alerta = document.createElement('div');
+        
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700','px-4','py-3','rounded','max-w-md','mx-auto','mt-6','text-center');
+        
+        alerta.innerHTML = `
+        <strong class="font-bold">Error</strong>
+        <span class="block">${mensaje}</span>
+        `;
+        
+        container.appendChild(alerta);
+
+        //se elimina la alerta despues de 5 segundos
+        setTimeout(() => {
+            alerta.remove();
+        }, 5000);
+    }
+}
+
+function consultarAPI(ciudad,pais){
+    const appId = '6d3121cbc6b5409a4fe8ec74d5ad9ad5'
+}
